ed60de975a991f75dcddbaee6395c34b4311c20d
FS-ichikawa <49864006+FS-ichikawa@users.noreply.github.com>
2020-11-04 14:15:12 +0900

    説明文の追記

--- README.md
+++ README.md
@@ -3,15 +3,8 @@
 
 node-redに読み込んでください。
 
-# 1 MQTT broker
+
+MQTT broker
 Aedes MQTT brokerを使用（moscaはサポート切れのため）
 必要に応じて処理ノードを追加してください。
 port:1883
-
-# 2 Topic:OneData
-一つデータ
-_index：sensors-sensor_aになります。
-
-# 3 Topic:TwoData
-2つデータ
-_index：sensors-sensor_a+sensor_bになります。
--- sample/sample.ino
+++ sample/sample.ino
@@ -2,44 +2,45 @@
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 
-// For random generation of client ID.
-static const char alphanum[] ="0123456789"
-                              "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                              "abcdefghijklmnopqrstuvwxyz"; 
-unsigned long prev;
-int fail_count = 0;
+//ランダムにclientIDを生成するための配列
+static const char alphanum[] = "0123456789"
+                               "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                               "abcdefghijklmnopqrstuvwxyz";
+
+unsigned long prev; //前回実行時間の記録
+int fail_count = 0; //失敗の回数をカウント
 WiFiClient espClient;
 PubSubClient client(espClient);
-char jsonOutput[128];
+char jsonOutput[128]; //publishするjsonを格納
 
 //====================================================
 const char *ssid = "yourSSID";
 const char *password = "yourPW";
-const char* mqtt_server = "youerIPAddress ex192.168.1.168";
+const char *mqtt_server = "youerIPAddress"; // Ex. 192.168.1.168
 const int port = 1883;
 
-const char* Topic = "TwoData";//OneData
+const char *Topic = "TwoData"; //OneData
 
-unsigned long interval = 300000;//5min
+unsigned long interval = 300000; //300000ms = 5min
 
-const char* device = "yourdevicename";
+const char *device = "yourdevicename";
 
-//小文字のみ
-const char* sensor_a = "yoursensorname";
-const char* sensor_b = "yoursensorname";
+//それぞれのセンサーの名前  小文字のみ
+const char *sensor_a = "yoursensorname";
+const char *sensor_b = "yoursensorname";
 
-const char* mqtt_username = "";
-const char* mqtt_password = "";
+//MQTTbrokerで指定したユーザー名とパスワード
+const char *mqtt_username = "";
+const char *mqtt_password = "";
 
 //====================================================
 
-
-
-
+//セットアップ
 void setup()
 {
   Serial.begin(115200);
-  while (!Serial);
+  while (!Serial)
+    ;
   Serial.println();
 
   setup_wifi();
@@ -54,7 +55,7 @@ void setup()
   prev = 0;
 }
 
-
+//ループ
 void loop()
 {
   if (WiFi.status() != WL_CONNECTED)
@@ -62,17 +63,19 @@ void loop()
     setup_wifi();
     prev = millis();
   }
-  if (!client.connected()) {
+  if (!client.connected())
+  {
     reconnect();
     prev = millis();
   }
   client.loop();
 
-  if ((millis() - prev) >= interval) {
-    float a = 1.0;
-    float b = 2.0;
-    publishJsonTwoData(a,sensor_a,b,sensor_b);
-    /*  Topic = OneData
+  if ((millis() - prev) >= interval)
+  {
+    float a = 1.0; //センサーAの値
+    float b = 2.0; //センサーBの値
+    publishJsonTwoData(a, sensor_a, b, sensor_b);
+    /*
      *  publishJsonOneData(a,sensor_a);
      *  publishJsonOneData(b,sensor_b);
      */
@@ -81,10 +84,19 @@ void loop()
   delay(1);
 }
 
-
 //送るデータによってfloatから変更する
 //JSON生成してPublishする
-void publishJsonOneData(float sensor_data_One,String sensor_One) {
+//
+/*
+{
+  "device":"device",
+  "sensor_One":"sensor_One",
+  "data_One":sensor_data_One,
+}
+*/
+void publishJsonOneData(float sensor_data_One, String sensor_One)
+{
+  char Topic = "OneData"; //
   const size_t CAPACITY = JSON_OBJECT_SIZE(4);
   StaticJsonDocument<CAPACITY> doc;
   JsonObject object = doc.to<JsonObject>();
@@ -92,7 +104,7 @@ void publishJsonOneData(float sensor_data_One,String sensor_One) {
   object["sensor"] = sensor_One;
   object["data"] = sensor_data_One;
   serializeJson(doc, jsonOutput);
-  //POSTする
+
   Serial.println(String(jsonOutput));
   client.publish(Topic, jsonOutput);
   fail_count = 0;
@@ -100,7 +112,18 @@ void publishJsonOneData(float sensor_data_One,String sensor_One) {
 
 //送るデータによってfloatから変更する
 //JSON生成してPublishする
-void publishJsonTwoData(float sensor_data_One,String sensor_One,float sensor_data_Two,String sensor_Two) {
+/*
+{
+  "device":"device",
+  "sensor_One":"sensor_One",
+  "data_One":sensor_data_One,
+  "sensor_Two":"humidity",
+  "data_Two":sensor_data_Two
+}
+*/
+void publishJsonTwoData(float sensor_data_One, String sensor_One, float sensor_data_Two, String sensor_Two)
+{
+  char Topic = "TwoData";
   const size_t CAPACITY = JSON_OBJECT_SIZE(7);
   StaticJsonDocument<CAPACITY> doc;
   JsonObject object = doc.to<JsonObject>();
@@ -110,22 +133,24 @@ void publishJsonTwoData(float sensor_data_One,String sensor_One,float sensor_dat
   object["sensor_Two"] = sensor_Two;
   object["data_Two"] = sensor_data_Two;
   serializeJson(doc, jsonOutput);
-  //POSTする
+
   Serial.println(String(jsonOutput));
   client.publish(Topic, jsonOutput);
   fail_count = 0;
 }
 
-
-void restart() {
-  if (120 <= fail_count++) {
+//失敗回数をカウント
+void restart()
+{
+  if (120 <= fail_count++)
+  {
     ESP.restart();
   }
 }
 
-
 //WiFiの接続
-void setup_wifi() {
+void setup_wifi()
+{
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED)
   {
@@ -141,35 +166,42 @@ void setup_wifi() {
 }
 
 //コールバック
-void callback(char* topic, byte* payload, unsigned int length) {
+void callback(char *topic, byte *payload, unsigned int length)
+{
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
-  for (int i = 0; i < length; i++) {
+  for (int i = 0; i < length; i++)
+  {
     Serial.print((char)payload[i]);
   }
   Serial.println();
 }
 
 //再接続
-void reconnect() {
+void reconnect()
+{
   char clientID[10];
-  
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
 
-    // Generate ClientID
-    for (int i = 0; i < 8; i++) {
-        clientID[i] = alphanum[random(51)];
+  //再接続までループ
+  while (!client.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+
+    // ClientID生成
+    for (int i = 0; i < 8; i++)
+    {
+      clientID[i] = alphanum[random(51)];
     }
     Serial.print("clientID : ");
     Serial.println(clientID);
-    
-    if (client.connect(clientID, mqtt_username, mqtt_password)) {
+
+    if (client.connect(clientID, mqtt_username, mqtt_password))
+    {
       Serial.println("connected");
-    } else {
+    }
+    else
+    {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
@@ -177,5 +209,6 @@ void reconnect() {
       delay(5000);
     }
     delay(10);
+    restart()
   }
 }
